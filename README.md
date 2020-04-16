527786744083c9c6901a1e9ad6318326c8947b3a
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-04-16 16:29:40 -0300

    Finish challenge

---
+++ README.md
@@ -0,0 +1,37 @@
+<p align="center">
+    <img alt="GoStack" src="https://ap.imagensbrasil.org/images/2020/04/09/banner-bootcamp-gostack-11.png" />
+</p>
+
+## :rocket: Desafio 04: Conceitos do React Native
+
+<br />
+<p align="center">
+<img alt="Projeto em funcionamento" src="https://media.giphy.com/media/h5oY1zqoJ1HwBiVHj9/giphy.gif">
+</p>
+
+✔ [Acessar página do desafio](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-react-native)
+
+🏁 Desafio completado em 16 de abril de 2020
+
+## ⚙️ Instalação
+
+```Bash
+# Instale as dependências deste projeto
+yarn
+
+# Execute a aplicação
+react-native run-android
+```
+
+> 🚧 Esta aplicação foi executada com êxito no Android, juntamente com o backend do seguinte repositório: https://github.com/guitexa/bootcamp-gostack-challenge-02
+
+## 🙂 Sobre o autor
+
+**Guilherme Teixeira**
+
+[![github](http://ap.imagensbrasil.org/images/2018/12/10/github-logo-1.png) ](https://github.com/guitexa)
+[![linkedin](http://ap.imagensbrasil.org/images/2018/12/10/linkedin-1.png)](https://www.linkedin.com/in/guitexa/)
+
+## Licença
+
+[MIT](./LICENSE) &copy; [Rocketseat](https://rocketseat.com.br/)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 import {
   SafeAreaView,
@@ -8,48 +8,106 @@ import {
   StatusBar,
   StyleSheet,
   TouchableOpacity,
-} from "react-native";
+} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  // Give a like to the repository and update state array
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`/repositories/${id}/like`);
+
+    const getIndex = repositories.findIndex(
+      (repository) => repository.id === id
+    );
+
+    repositories[getIndex].likes = response.data.likes;
+
+    setRepositories([...repositories]);
   }
 
+  // Delete repository and update state array
+  function handleRemoveRepository(id) {
+    api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
+  }
+
+  // Create a new repository and update state array
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      title: `Repo #${Date.now()}`,
+      url: 'http://api.github.com',
+      techs: ['NodeJS', 'React', 'React Native'],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  // List existing repositories on state array
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle="light-content" backgroundColor="#333" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          keyExtractor={repositories.id}
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
+              <View style={styles.techsContainer}>
+                {repository.techs.map((tech) => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                ))}
+              </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  ❤ {repository.likes}{' '}
+                  {repository.likes > 1 ? 'curtidas' : 'curtida'}
+                </Text>
+              </View>
+              <View style={styles.containerButtons}>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonTextLike}>Curtir</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.button}
+                  onPress={() => handleRemoveRepository(repository.id)}
+                >
+                  <Text style={styles.buttonTextDelete}>Remover</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        ></FlatList>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.buttonAdd}
+          onPress={handleAddRepository}
+        >
+          <Text style={styles.buttonTextAdd}>Adicionar</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -58,50 +116,81 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: '#333',
   },
   repositoryContainer: {
-    marginBottom: 15,
+    marginTop: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
+    backgroundColor: '#f1f1f1',
     padding: 20,
+    borderRadius: 4,
   },
   repository: {
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 25,
+    fontWeight: 'bold',
   },
   techsContainer: {
-    flexDirection: "row",
-    marginTop: 10,
+    flexDirection: 'row',
+    marginTop: 6,
   },
   tech: {
     fontSize: 12,
-    fontWeight: "bold",
-    marginRight: 10,
-    backgroundColor: "#04d361",
-    paddingHorizontal: 10,
+    fontWeight: 'bold',
+    marginRight: 5,
+    backgroundColor: '#00a02a',
+    paddingHorizontal: 12,
     paddingVertical: 5,
-    color: "#fff",
+    color: '#fff',
+    borderRadius: 14,
   },
   likesContainer: {
     marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    marginBottom: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   likeText: {
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
+  },
+  containerButtons: {
+    flexDirection: 'row',
   },
   button: {
     marginTop: 10,
   },
-  buttonText: {
-    fontSize: 14,
-    fontWeight: "bold",
+  buttonTextLike: {
+    fontSize: 15,
+    backgroundColor: '#7159c1',
+    fontWeight: 'bold',
+    marginRight: 15,
+    color: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 5,
+  },
+  buttonTextDelete: {
+    fontSize: 15,
+    backgroundColor: '#ca4949',
+    fontWeight: 'bold',
+    color: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonAdd: {
+    margin: 20,
+  },
+  buttonTextAdd: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
+    color: '#fff',
+    backgroundColor: '#7159c1',
     padding: 15,
+    borderRadius: 5,
   },
 });
